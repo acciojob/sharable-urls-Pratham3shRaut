2d3b61d6a0b9408873747d92ec4567a05d298884
Pratham3shRaut <132664521+Pratham3shRaut@users.noreply.github.com>
2025-12-07 11:52:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// your code here
+const button = document.getElementById("button");
+const urlHeading = document.getElementById("url");
+
+button.addEventListener("click", () => {
+  const name = document.getElementById("name").value.trim();
+  const year = document.getElementById("year").value.trim();
+
+  let base = "https://localhost:8080/";
+  let query = [];
+
+  if (name) query.push(`name=${encodeURIComponent(name)}`);
+  if (year) query.push(`year=${encodeURIComponent(year)}`);
+
+  if (query.length > 0) {
+    base += "?" + query.join("&");
+  }
+
+  urlHeading.textContent = base;
+});
